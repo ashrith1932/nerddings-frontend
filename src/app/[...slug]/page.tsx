871404e88ae1d8f4d373c4f263fe26f2a569d761
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 14:33:10 +0530

    feat: add route skeleton to all application routes

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -2,7 +2,9 @@ import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
+import PageSkeleton from "@/components/ui/PageSkeleton";
+import RouteTransitionSkeleton from "@/components/ui/RouteTransitionSkeleton";
 
 export default function CatchAllPage() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><PageSkeleton /><RouteTransitionSkeleton /></>;
 }
