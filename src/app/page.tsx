42e451504004ce67aeaab8f7381813ad4a40c144
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 20:11:09 +0530

    fix: use single client app gate for root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { NerddingApp } from "@/components/layout/NerddingApp";
-import SocialEnhancer from "@/components/social/SocialEnhancer";
-import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
-import LiveNavCounts from "@/components/social/LiveNavCounts";
-import ProfileEditorOverlay from "@/components/profile/ProfileEditorOverlay";
+import ClientAppGate from "./ClientAppGate";
 
 export default function Page() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><ProfileEditorOverlay /></>;
+  return <ClientAppGate />;
 }
