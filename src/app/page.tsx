9b976f28b9cbabc960ac5e8474043452e870de72
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 12:58:34 +0530

    feat: keep live nav counts in original shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
+import LiveNavCounts from "@/components/social/LiveNavCounts";
 
 export default function Page() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /></>;
 }
