62a94ec30574099ff0a176472035b921d6786f55
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 14:33:03 +0530

    feat: add route skeleton to root surface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
 import PageSkeleton from "@/components/ui/PageSkeleton";
+import RouteTransitionSkeleton from "@/components/ui/RouteTransitionSkeleton";
 
 export default function Page() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><PageSkeleton /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><PageSkeleton /><RouteTransitionSkeleton /></>;
 }
