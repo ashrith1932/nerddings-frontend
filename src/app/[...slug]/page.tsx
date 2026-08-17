00c803b2df9858eabc446dc4b8194f67b5555472
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 18:42:41 +0530

    feat: mount agent verification gate

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -2,7 +2,8 @@ import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
+import AgentVerificationGate from "@/components/agent/AgentVerificationGate";
 
 export default function CatchAllPage() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><AgentVerificationGate /></>;
 }
