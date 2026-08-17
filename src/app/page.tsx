77a6dd02e4f37a98f670f53f6e70f8aa696df555
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 14:23:09 +0530

    feat: show route skeleton across social pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
+import PageSkeleton from "@/components/ui/PageSkeleton";
 
 export default function Page() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><PageSkeleton /></>;
 }
