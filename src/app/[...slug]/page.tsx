7c9ff287b3d963db2f91fc54c4e6ff31f616e374
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 19:42:23 +0530

    fix: reliably mount live social enhancer

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
+import ReliableSocialEnhancer from "@/components/social/ReliableSocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
 import AgentVerificationGate2 from "@/components/agent/AgentVerificationGate2";
@@ -9,5 +10,5 @@ import AgentPendingNotice from "@/components/agent/AgentPendingNotice";
 import ProfileEditorOverlay from "@/components/profile/ProfileEditorOverlay";
 
 export default function CatchAllPage() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><AgentVerificationGate2 /><AgentVerificationRedirect /><AgentLoginLink /><AgentPendingNotice /><ProfileEditorOverlay /></>;
+  return <><NerddingApp /><SocialEnhancer /><ReliableSocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><AgentVerificationGate2 /><AgentVerificationRedirect /><AgentLoginLink /><AgentPendingNotice /><ProfileEditorOverlay /></>;
 }
