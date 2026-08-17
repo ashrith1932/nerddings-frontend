e7ab5fcfc53e1a02ca7705bdec62ea136e0b16b3
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-17 19:18:41 +0530

    feat: mount profile editor and visual polish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { NerddingApp } from "@/components/layout/NerddingApp";
 import SocialEnhancer from "@/components/social/SocialEnhancer";
 import SocialProfileRedirector from "@/components/social/SocialProfileRedirector";
 import LiveNavCounts from "@/components/social/LiveNavCounts";
+import ProfileEditorOverlay from "@/components/profile/ProfileEditorOverlay";
 
 export default function Page() {
-  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /></>;
+  return <><NerddingApp /><SocialEnhancer /><SocialProfileRedirector /><LiveNavCounts /><ProfileEditorOverlay /></>;
 }
