2171a25e623802e38af346129074feea6faf202e
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2026-08-19 19:10:46 +0530

    Fix catch-all hydration mismatch

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,4 +1,9 @@
-import NerddingApp from "@/components/layout/NerddingApp";
+import dynamic from "next/dynamic";
+
+const NerddingApp = dynamic(
+  () => import("@/components/layout/NerddingApp").then((module) => module.default),
+  { ssr: false },
+);
 
 export default function CatchAllPage() {
   return <NerddingApp />;
